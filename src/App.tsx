bd7a49f5ad452eadf91be1c678f383926a5e8e07
OctopusRoe <zp393939@outlook.com>
2023-07-10 16:29:07 +0800

    fix: auto format

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
+/*
+ * @Author: OctopusRoe
+ * @Date: 2023-07-10 14:44:55
+ * @LastEditors: OctopusRoe
+ * @LastEditTime: 2023-07-10 16:25:44
+ * @Description:
+ */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -5,12 +5,12 @@
  * @LastEditTime: 2023-07-10 11:50:13
  * @Description:
  */
-declare module "*.svg";
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.gif";
-declare module "*.bmp";
-declare module "*.tiff";
-declare module "*.less";
-declare module "*.css";
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+declare module '*.less';
+declare module '*.css';
